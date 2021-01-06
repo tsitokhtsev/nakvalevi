bac08392302998c7baa1969a051fd287339435d1
tsitokhtsev <tsitokhtsev2002@gmail.com>
2021-01-07 00:03:45 +0400

    optimized home page

--- js/main.js
+++ js/main.js
@@ -7,6 +7,19 @@ menuBtn.addEventListener('click', function () {
     navbar.classList.toggle('navbar-show')
 })
 
+// title animation on home page
+const heroTitleWords = document.getElementsByClassName('home-hero-title-word')
+
+setTimeout(function () {
+    heroTitleWords[0].classList.add('home-hero-title-word-show')
+}, 500);
+setTimeout(function () {
+    heroTitleWords[1].classList.add('home-hero-title-word-show')
+}, 600)
+setTimeout(function () {
+    heroTitleWords[2].classList.add('home-hero-title-word-show')
+}, 700)
+
 // back-to-top button
 const backToTopBtn = document.querySelector('.back-to-top')
 
@@ -30,10 +43,10 @@ backToTopBtn.addEventListener('click', function () {
 // timeline anchors
 const anchors = document.querySelector('.navbar-anchors')
 
-setTimeout(function() {
+setTimeout(function () {
     anchors.classList.add('navbar-anchors-show')
 }, 500);
-setTimeout(function() {
+setTimeout(function () {
     anchors.classList.remove('navbar-anchors-show')
 }, 1500);
 
