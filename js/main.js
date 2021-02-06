9ec5e033eb3cd3742d4086d625fd26ec9202227b
tsitokhtsev <tsitokhtsev2002@gmail.com>
2021-02-06 18:24:02 +0400

    updated navbar on smartphones

--- js/main.js
+++ js/main.js
@@ -3,10 +3,23 @@ const menuBtn = document.querySelector('.logo-menu')
 const navbar = document.querySelector('.navbar')
 const article = document.querySelector('.article')
 
+
 menuBtn.addEventListener('click', function () {
+    console.log(window.pageYOffset);
     menuBtn.classList.toggle('logo-menu-close')
     navbar.classList.toggle('navbar-show')
-    article.classList.toggle('article-show')
+    if (window.pageYOffset == 0 && menuBtn.classList.contains('logo-menu-close')) {
+        article.classList.add('article-show')
+    }
+    else {
+        article.classList.remove('article-show')
+    }
+})
+
+window.addEventListener('scroll', function(){
+    if (window.pageYOffset && menuBtn.classList.contains('logo-menu-close')){
+        article.classList.add('article-show')
+    }
 })
 
 // back-to-top button
