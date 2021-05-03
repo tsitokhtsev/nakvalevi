e6d1186e4bee5f9b3884113081450b07fc9634c8
tsitokhtsev <tsitokhtsev2002@gmail.com>
2021-05-03 16:33:50 +0400

    updated search, writings' pages now rendering from data.json

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-// menu
+/* Menu */
 const menuBtn = document.querySelector('.logo-menu-colored')
 const navbar = document.querySelector('.navbar')
 const article = document.querySelector('.article')
 
-
-menuBtn.addEventListener('click', function () {
+menuBtn.onclick = () => {
     menuBtn.classList.toggle('logo-menu-colored-close')
     navbar.classList.toggle('navbar-show')
     if (window.pageYOffset == 0 && menuBtn.classList.contains('logo-menu-colored-close')) {
@@ -13,7 +12,7 @@ menuBtn.addEventListener('click', function () {
     else {
         article.classList.remove('article-show')
     }
-})
+}
 
 window.addEventListener('scroll', function () {
     if (window.pageYOffset && menuBtn.classList.contains('logo-menu-colored-close')) {
@@ -23,121 +22,123 @@ window.addEventListener('scroll', function () {
 
 /* Search */
 let searchItems = []
-fetch("/js/search-items.json", {
+fetch("/js/data.json", {
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
     }
-}).then(data => data.json()).then(data => searchItems = data)
+}).then(data => data.json()).then(data => {
+    searchItems = data
 
-const body = document.querySelector('.body')
-const search = document.querySelector('.search')
-const searchBtn = document.querySelector('.logo-search')
-const searchBtnCol = document.querySelector('.logo-search-colored')
-const searchInput = document.querySelector('.search-bar-input')
-const searchClear = document.querySelector('.search-bar-clear')
-const searchItemsSection = document.querySelector('.search-section')
-const searchNoMatch = document.querySelector('.search-section-nomatch')
+    const body = document.querySelector('.body')
+    const search = document.querySelector('.search')
+    const searchBtn = document.querySelector('.logo-search')
+    const searchBtnCol = document.querySelector('.logo-search-colored')
+    const searchInput = document.querySelector('.search-bar-input')
+    const searchClear = document.querySelector('.search-bar-clear')
+    const searchItemsSection = document.querySelector('.search-section')
+    const searchNoMatch = document.querySelector('.search-section-nomatch')
 
-searchBtn.addEventListener('click', function () {
-    searchShow()
-})
-searchBtnCol.addEventListener('click', function () {
-    searchShow()
-})
+    searchBtn.onclick = () => searchShow()
+    searchBtnCol.onclick = () => searchShow()
 
-function searchShow() {
-    if (window.pageYOffset < 65 && window.innerWidth > 480) {
+    searchShow = () => {
+        if (window.pageYOffset < 65 && window.innerWidth > 480) {
+            document.documentElement.scrollTo({
+                top: 65,
+                behavior: "smooth"
+            })
+        }
+
+        search.classList.toggle('search-show')
+
+        if (window.innerWidth < 480) {
+            search.style.top = '60px'
+            search.style.height = `${window.innerHeight - 60}px`
+        } else {
+            search.style.top = '45px'
+            search.style.height = `${window.innerHeight - 45}px`
+            body.classList.toggle('stop-scrolling')
+        }
+
+        searchInput.focus()
+    }
+
+    searchInput.oninput = function () {
+        let text = this.value.trim()
+
+        let displaySearchItems = []
+        searchItems.forEach(period => period.forEach(author => author.writings.map(writing => {
+            if (writing.name.includes(text) || author.name.includes(text) || author.surname.includes(text)) {
+                displaySearchItems.push(`<a href="${writing.link}">
+    <p class="search-section-item">${writing.name} - ${author.name} ${author.surname}</p>
+</a>`)
+            }
+        })))
+        displaySearchItems = displaySearchItems.join('\n')
+        searchItemsSection.innerHTML = displaySearchItems
+
+        searchItemsSection.classList.add('search-section-show')
+        let output = document.querySelectorAll('.search-section a')
+        let outputItems = document.querySelectorAll('.search-section-item')
+
+        if (text != '') {
+            let noMatch = []
+            outputItems.forEach(elem => {
+                if (elem.innerText.search(text) == -1) {
+                    elem.style.display = 'none'
+                    noMatch.push(elem)
+                }
+            })
+            if (output.length == noMatch.length) {
+                let noMatchMsg = document.createElement('p')
+                noMatchMsg.setAttribute('class', 'search-section-nomatch')
+                searchItemsSection.hasChildNodes() ? searchItemsSection.insertBefore(noMatchMsg, searchItemsSection.lastChild.nextSibling) : searchItemsSection.insertBefore(noMatchMsg, searchItemsSection.lastChild)
+                noMatchMsg.innerHTML = 'არაფერი მოიძებნა'
+            }
+        } else {
+            outputItems.forEach(elem => {
+                elem.style.display = 'none'
+            })
+            searchItemsSection.classList.remove('search-section-show')
+        }
+    }
+
+    searchClear.onclick = () => {
+        searchInput.value = ''
+        searchItemsSection.classList.remove('search-section-show')
+        searchInput.focus()
+    }
+
+    // navbar link hover
+    const navbarLink = document.querySelectorAll('.navbar-link')
+
+    navbarLink.forEach(function (link) {
+        link.addEventListener('mouseover', function () {
+            link.querySelector('.navbar-link-title-hr').classList.add('navbar-link-title-hr-show')
+        })
+        link.addEventListener('mouseout', function () {
+            link.querySelector('.navbar-link-title-hr').classList.remove('navbar-link-title-hr-show')
+        })
+    })
+
+    /* Back-To-Top Button */
+    const backToTopBtn = document.querySelector('.back-to-top')
+
+    document.onscroll = () => {
+        const clientHeight = document.documentElement.clientHeight
+        const scrollTop = window.pageYOffset
+        if (scrollTop > clientHeight * 0.4) {
+            backToTopBtn.classList.add("show-btn")
+        } else {
+            backToTopBtn.classList.remove("show-btn")
+        }
+    }
+
+    backToTopBtn.onclick = () => {
         document.documentElement.scrollTo({
-            top: 65,
+            top: 0,
             behavior: "smooth"
         })
     }
-
-    search.classList.toggle('search-show')
-
-    if (window.innerWidth < 480) {
-        search.style.top = '60px'
-        search.style.height = `${window.innerHeight - 60}px`
-    } else {
-        search.style.top = '45px'
-        search.style.height = `${window.innerHeight - 45}px`
-        body.classList.toggle('stop-scrolling')
-    }
-
-    searchInput.focus()
-}
-
-searchInput.addEventListener('input', function () {
-    let text = this.value.trim();
-    diplaySearchItems();
-    searchItemsSection.classList.add('search-section-show')
-    let output = document.querySelectorAll('.search-section a')
-    let outputItems = document.querySelectorAll('.search-section-item')
-
-    if (text != '') {
-        outputItems.forEach(function (elem) {
-            if (elem.innerText.search(text) == -1) {
-                elem.style.display = 'none'
-                elem.classList.add('no-match')
-            }
-        })
-        let noMatch = document.querySelectorAll('.no-match')
-        if (output.length == noMatch.length) {
-            searchItemsSection.innerHTML = '<p class="search-section-nomatch">არაფერი მოიძებნა</p>'
-        }
-    } else {
-        outputItems.forEach(function (elem) {
-            elem.style.display = 'none'
-        })
-        searchItemsSection.classList.remove('search-section-show')
-    }
-})
-
-function diplaySearchItems() {
-    let displaySearchItems = searchItems.map(function (item) {
-        return `<a href="${item.link}">
-                    <p class="search-section-item">${item.name}</p>
-                </a>`
-    })
-    displaySearchItems = displaySearchItems.join('')
-    searchItemsSection.innerHTML = displaySearchItems
-}
-
-searchClear.addEventListener('click', function () {
-    searchInput.value = ''
-    searchItemsSection.classList.remove('search-section-show')
-    searchInput.focus()
-})
-
-// navbar link hover
-const navbarLink = document.querySelectorAll('.navbar-link')
-
-navbarLink.forEach(function (link) {
-    link.addEventListener('mouseover', function () {
-        link.querySelector('.navbar-link-title-hr').classList.add('navbar-link-title-hr-show')
-    })
-    link.addEventListener('mouseout', function () {
-        link.querySelector('.navbar-link-title-hr').classList.remove('navbar-link-title-hr-show')
-    })
-})
-
-// back-to-top button
-const backToTopBtn = document.querySelector('.back-to-top')
-
-document.addEventListener('scroll', function () {
-    const clientHeight = document.documentElement.clientHeight
-    const scrollTop = window.pageYOffset
-    if (scrollTop > clientHeight * 0.4) {
-        backToTopBtn.classList.add("show-btn")
-    } else {
-        backToTopBtn.classList.remove("show-btn")
-    }
-})
-
-backToTopBtn.addEventListener('click', function () {
-    document.documentElement.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    })
 })
--- js/writing.js
+++ js/writing.js
@@ -1,46 +1,160 @@
-// writing page content
-const cards = document.querySelectorAll('.writing-card')
+/* Writing Page Content */
+const container = document.querySelector('.writing.article').querySelector('.container')
 
-window.addEventListener('DOMContentLoaded', () => {
-    const firstCard = cards[0]
-    const btn = firstCard.querySelector('.writing-card-button')
-    const content = firstCard.querySelector('.writing-card-content')
+fetch('/js/data.json', {
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+}).then(data => data.json()).then(data => {
+    data.forEach(p => p.map(a => {
+        if (window.location.href.includes(a.link)) {
+            a.writings.map(w => {
+                if (window.location.href.includes(w.link)) {
+                    container.innerHTML =
+                        `<h1 class="title">
+                            <span class="title-text">${w.name}</span>
+                        </h1>
+                        <div class="writing-info section">
+                            <p class="writing-info-block">
+                                <span class="writing-info-block-name PURPLE">ავტორი:</span>
+                                <span class="BLACK"><a href="${a.link}">${a.name} ${a.surname}</a></span>
+                            </p>
+                            <p class="writing-info-block">
+                                <span class="writing-info-block-name PURPLE">წელი:</span>
+                                <span class="BLACK">${w.year == '~' ? a.year : w.year}</span>
+                            </p>
+                            <p class="writing-info-block">
+                                <span class="writing-info-block-name PURPLE">ჟანრი:</span>
+                                <span class="BLACK">${w.genre}</span>
+                            </p>
+                        </div>
+                        
+                        <div class="section">
+                            <div class="writing-card">
+                                <div class="writing-card-header">
+                                    <div class="writing-card-title">
+                                        <span class="writing-card-title-text">ანალიზი</span>
+                                    </div>
+                                    <button class="writing-card-button"></button>
+                                </div>
+                                <div class="writing-card-content">
+                                    <div class="writing-analysis"></div>
+                                </div>
+                            </div>
+                        
+                            <div class="writing-card">
+                                <div class="writing-card-header">
+                                    <div class="writing-card-title">
+                                        <span class="writing-card-title-text">პერსონაჟები</span>
+                                    </div>
+                                    <button class="writing-card-button"></button>
+                                </div>
+                                <div class="writing-card-content">
+                                    <div class="writing-characters"></div>
+                                </div>
+                            </div>
+                        
+                            <div class="writing-card">
+                                <div class="writing-card-header">
+                                    <div class="writing-card-title">
+                                        <span class="writing-card-title-text">ლექსიკონი</span>
+                                    </div>
+                                    <button class="writing-card-button"></button>
+                                </div>
+                                <div class="writing-card-content">
+                                    <div class="writing-dictionary"></div>
+                                </div>
+                            </div>
+                        </div>`
 
-    btn.classList.add('writing-card-button-show')
-    firstCard.style.maxHeight = `${content.clientHeight + 45}px`
-    content.classList.add('writing-card-content-show')
-})
+                    const analysis = container.querySelector('.writing-analysis')
+                    if (w.essay != '') {
+                        w.essay.forEach(e => {
+                            let paragraph = document.createElement('p')
+                            analysis.hasChildNodes() ? analysis.insertBefore(paragraph, analysis.lastChild.nextSibling) : analysis.insertBefore(paragraph, analysis.lastChild)
+                            paragraph.innerHTML = `${e}`
+                        })
+                    } else {
+                        analysis.innerHTML = '<span class="na">არ არის ინფორმაცია</span>'
+                    }
 
-cards.forEach((card) => {
-    const header = card.querySelector('.writing-card-header')
-    
-    header.addEventListener('click', () => {
-        cards.forEach((item) => {
-            if (item != card) {
-                const btn = item.querySelector('.writing-card-button')
-                const content = item.querySelector('.writing-card-content')
-                
-                item.style.maxHeight = '45px'
-                btn.classList.remove('writing-card-button-show')
-                content.classList.remove('writing-card-content-show')
-            }
-        })
-        
-        const btn = card.querySelector('.writing-card-button')
-        const content = card.querySelector('.writing-card-content')
+                    const characters = container.querySelector('.writing-characters')
+                    if (w.characters != '') {
+                        w.characters.forEach(c => {
+                            let character = document.createElement('div')
+                            character.setAttribute('class', 'writing-characters-character')
+                            characters.hasChildNodes() ? characters.insertBefore(character, characters.lastChild.nextSibling) : characters.insertBefore(character, characters.lastChild)
+                            character.innerHTML = `
+                                <p class="writing-characters-character-name PURPLE">${c.name}</p>
+                                <ul></ul>`
 
-        btn.classList.toggle('writing-card-button-show')
-        if (btn.classList.contains('writing-card-button-show')) {
-            card.style.maxHeight = `${content.clientHeight + 45}px`
-        } else {
-            card.style.maxHeight = '45px'
-        }
-        content.classList.toggle('writing-card-content-show')
-        setTimeout(() => {
-            const cardPosition = card.getBoundingClientRect()
-            window.scrollTo({
-                top: window.pageYOffset + cardPosition.top - 75
+                            const qualities = character.querySelector('ul')
+                            c.qualities.forEach(q => {
+                                let quality = document.createElement('li')
+                                qualities.hasChildNodes() ? qualities.insertBefore(quality, qualities.lastChild.nextSibling) : qualities.insertBefore(quality, qualities.lastChild)
+                                quality.innerHTML = `<span>${q};</span>`
+                            })
+                        })
+                    } else {
+                        characters.innerHTML = '<span class="na">არ არის ინფორმაცია</span>'
+                    }
+
+                    const dictionary = container.querySelector('.writing-dictionary')
+                    if (w.dictionary != '') {
+                        w.dictionary.forEach(w => {
+                            let word = document.createElement('span')
+                            word.setAttribute('class', 'writing-dictionary-word')
+                            dictionary.hasChildNodes() ? dictionary.insertBefore(word, dictionary.lastChild.nextSibling) : dictionary.insertBefore(word, dictionary.lastChild)
+                            word.innerHTML = `<span class="PURPLE">${w.word}</span><br>${w.meaning}`
+                        })
+                    } else {
+                        dictionary.innerHTML = '<span class="na">არ არის ინფორმაცია</span>'
+                    }
+
+                    const cards = document.querySelectorAll('.writing-card')
+
+                    const btnFirst = cards[0].querySelector('.writing-card-button')
+                    const contentFirst = cards[0].querySelector('.writing-card-content')
+
+                    btnFirst.classList.add('writing-card-button-show')
+                    cards[0].style.maxHeight = `${contentFirst.clientHeight + 45}px`
+                    contentFirst.classList.add('writing-card-content-show')
+
+                    cards.forEach(card => {
+                        const header = card.querySelector('.writing-card-header')
+
+                        header.onclick = () => {
+                            cards.forEach(item => {
+                                if (item != card) {
+                                    const btn = item.querySelector('.writing-card-button')
+                                    const content = item.querySelector('.writing-card-content')
+
+                                    item.style.maxHeight = '45px'
+                                    btn.classList.remove('writing-card-button-show')
+                                    content.classList.remove('writing-card-content-show')
+                                }
+                            })
+
+                            const btn = card.querySelector('.writing-card-button')
+                            const content = card.querySelector('.writing-card-content')
+
+                            btn.classList.toggle('writing-card-button-show')
+                            if (btn.classList.contains('writing-card-button-show')) {
+                                card.style.maxHeight = `${content.clientHeight + 45}px`
+                            } else {
+                                card.style.maxHeight = '45px'
+                            }
+                            content.classList.toggle('writing-card-content-show')
+                            setTimeout(() => {
+                                const cardPosition = card.getBoundingClientRect()
+                                window.scrollTo({ top: window.pageYOffset + cardPosition.top - 75 })
+                            }, 500)
+                        }
+                    })
+                }
+
             })
-        }, 500)
-    })
+        }
+    }))
 })
