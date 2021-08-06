3c2e8d20dc677ed117bea14faa6fdcf0820ba001
tsitokhtsev <tsitokhtsev2002@gmail.com>
2021-08-06 18:36:30 +0400

    Updates: - Set up hero on home page - Downloaded fonts

--- src/Home/Hero/Hero.js
+++ src/Home/Hero/Hero.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import './hero.scss'
 
 const Hero = () => {
+	// if (window.innerWidth < 480) {
+	// 	let heroHight = window.innerHeight - 90
+	// 	hero.style.height = `${heroHight}px`
+	// 	heroContainerBottom.style.height = `${heroHight - 225}px`
+	// } else {
+	// 	hero.style.height = `${window.innerHeight - 190}px`
+	// }
+
 	return (
-		<div className="hero section">
-			Hero
+		<div className="hero section" style={{ height: window.innerHeight - 220 }}>
+			<div className="hero-container">
+				<h1 className="hero-title">
+					<p className="hero-title-word">გავკვალოთ</p>
+					<p className="hero-title-word">ცოდნის</p>
+					<p className="hero-title-word">გზა</p>
+				</h1>
+				<div className="hero-container-right"></div>
+				<div className="hero-container-bottom"></div>
+			</div>
 		</div>
 	)
 }
