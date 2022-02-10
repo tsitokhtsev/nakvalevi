44cc851485b44d4a0967ca747f8b0ed53ac49bcb
tsitokhtsev <tsitokhtsev2002@gmail.com>
2022-02-10 23:05:05 +0400

    Updated dependencies, react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,49 @@
-import React, { useState } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import Header from './Header'
-import Navbar from './Navbar'
-import Container from './Container'
-import Home from '../pages/Home'
-import Timeline from '../pages/Timeline'
-import About from '../pages/About'
-import Author from '../pages/Author'
-import Writing from '../pages/Writing'
-import Footer from './Footer'
+import Header from './Header';
+import Navbar from './Navbar';
+import Container from './Container';
+import Home from '../pages/Home';
+import Timeline from '../pages/Timeline';
+import About from '../pages/About';
+import Author from '../pages/Author';
+import Writing from '../pages/Writing';
+import Footer from './Footer';
 
-import '../style/app.scss'
+import '../style/app.scss';
 
 const App = () => {
-	const [navbarExpanded, setNavbarExpanded] = useState(false)
-	const [scroll, setScroll] = useState(window.pageYOffset)
+	const [navbarExpanded, setNavbarExpanded] = useState(false);
+	const [scroll, setScroll] = useState(window.pageYOffset);
 
-	window.onscroll = () => setScroll(window.pageYOffset)
+	window.onscroll = () => setScroll(window.pageYOffset);
 
 	return (
 		<div
 			className="app"
 			style={{ paddingTop: navbarExpanded && scroll < 5 ? '85px' : '40px' }}
 		>
-			<BrowserRouter>
-				<Header
-					navbarExpanded={navbarExpanded}
-					setNavbarExpanded={setNavbarExpanded}
-				/>
-				<Navbar
-					navbarExpanded={navbarExpanded}
-					setNavbarExpanded={setNavbarExpanded}
-				/>
-				<Container>
-					<Route path="/" exact component={Home} />
-					<Route path="/timeline" exact component={Timeline} />
-					<Route path="/about" exact component={About} />
-					<Route path="/:authorName-:authorSurname" exact component={Author} />
-					<Route path="/:authorName-:authorSurname/:writingName" exact component={Writing} />
-				</Container>
-				<Footer />
-			</BrowserRouter>
+			<Header
+				navbarExpanded={navbarExpanded}
+				setNavbarExpanded={setNavbarExpanded}
+			/>
+			<Navbar
+				navbarExpanded={navbarExpanded}
+				setNavbarExpanded={setNavbarExpanded}
+			/>
+			<Container>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/timeline" element={<Timeline />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/:authorName-:authorSurname" element={<Author />} />
+					<Route path="/:authorName-:authorSurname/:writingName" element={<Writing />} />
+				</Routes>
+			</Container>
+			<Footer />
 		</div>
-	)
-}
+	);
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from './components/App'
-import reducers from './reducers'
+import App from './components/App';
+import reducers from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
 	reducers,
 	composeEnhancers(applyMiddleware(thunk))
-)
+);
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</Provider>,
-  document.getElementById('root')
-)
+	document.getElementById('root')
+);
