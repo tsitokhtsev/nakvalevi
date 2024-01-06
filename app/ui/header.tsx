b0f1b9c268c6e966467e2d3072a379433aac572d
Grigoli Tsitokhtsevi <tsitokhtsev2002@gmail.com>
2024-01-06 19:03:08 +0400

    NAK-50 Header: adjsted home href, removed pathname and link text color

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -2,7 +2,6 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { usePathname } from 'next/navigation';
 
 const links = [
     { name: 'მთავარი', href: '/' },
@@ -11,11 +10,9 @@ const links = [
 ];
 
 export default function Header() {
-    const pathname = usePathname();
-
     return (
         <header className="flex justify-between items-center mx-auto p-4 max-w-screen-xl">
-            <Link href={'/'}>
+            <Link href="/">
                 <Image
                     src="/images/logo.svg"
                     alt="ნაკვალევის ლოგო"
@@ -26,11 +23,7 @@ export default function Header() {
             <nav className="flex gap-6">
                 {links.map((link) => {
                     return (
-                        <Link
-                            key={link.name}
-                            href={link.href}
-                            className="text-primary"
-                        >
+                        <Link key={link.name} href={link.href}>
                             {link.name.toUpperCase()}
                         </Link>
                     );
