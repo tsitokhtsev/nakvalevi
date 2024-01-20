f5020e349be991fb0660de68408044f10969cb54
Grigoli Tsitokhtsevi <tsitokhtsev2002@gmail.com>
2024-01-20 23:12:25 +0400

    NAK-51 Updated table relations, created writings_genres table

--- app/lib/database.ts
+++ app/lib/database.ts
@@ -1,10 +1,9 @@
-
 export type Json =
     | string
     | number
     | boolean
     | null
-    | { [key: string]: Json | undefined; }
+    | { [key: string]: Json | undefined }
     | Json[];
 
 export interface Database {
@@ -16,7 +15,6 @@ export interface Database {
                     image: string;
                     name: string;
                     period: number;
-                    surname: string;
                     year: string;
                 };
                 Insert: {
@@ -24,7 +22,6 @@ export interface Database {
                     image: string;
                     name: string;
                     period: number;
-                    surname: string;
                     year: string;
                 };
                 Update: {
@@ -32,7 +29,6 @@ export interface Database {
                     image?: string;
                     name?: string;
                     period?: number;
-                    surname?: string;
                     year?: string;
                 };
                 Relationships: [];
@@ -64,11 +60,11 @@ export interface Database {
                 };
                 Relationships: [
                     {
-                        foreignKeyName: "essays_writingId_fkey";
-                        columns: ["writingId"];
+                        foreignKeyName: 'essays_writingId_fkey';
+                        columns: ['writingId'];
                         isOneToOne: false;
-                        referencedRelation: "writings";
-                        referencedColumns: ["id"];
+                        referencedRelation: 'writings';
+                        referencedColumns: ['id'];
                     }
                 ];
             };
@@ -92,42 +88,72 @@ export interface Database {
             };
             writings: {
                 Row: {
-                    authorId: number;
-                    genres: number[];
+                    author_id: number;
                     id: number;
-                    name: string;
                     period: number;
+                    title: string;
                     year: string;
                 };
                 Insert: {
-                    authorId: number;
-                    genres: number[];
+                    author_id: number;
                     id?: number;
-                    name: string;
                     period: number;
+                    title: string;
                     year: string;
                 };
                 Update: {
-                    authorId?: number;
-                    genres?: number[];
+                    author_id?: number;
                     id?: number;
-                    name?: string;
                     period?: number;
+                    title?: string;
                     year?: string;
                 };
                 Relationships: [
                     {
-                        foreignKeyName: "writings_authorId_fkey";
-                        columns: ["authorId"];
+                        foreignKeyName: 'writings_author_id_fkey';
+                        columns: ['author_id'];
                         isOneToOne: false;
-                        referencedRelation: "authors";
-                        referencedColumns: ["id"];
+                        referencedRelation: 'authors';
+                        referencedColumns: ['id'];
+                    }
+                ];
+            };
+            writings_genres: {
+                Row: {
+                    created_at: string;
+                    genre_id: number;
+                    writing_id: number;
+                };
+                Insert: {
+                    created_at?: string;
+                    genre_id: number;
+                    writing_id: number;
+                };
+                Update: {
+                    created_at?: string;
+                    genre_id?: number;
+                    writing_id?: number;
+                };
+                Relationships: [
+                    {
+                        foreignKeyName: 'writings_genres_genre_id_fkey';
+                        columns: ['genre_id'];
+                        isOneToOne: false;
+                        referencedRelation: 'genres';
+                        referencedColumns: ['id'];
+                    },
+                    {
+                        foreignKeyName: 'writings_genres_writing_id_fkey';
+                        columns: ['writing_id'];
+                        isOneToOne: false;
+                        referencedRelation: 'writings';
+                        referencedColumns: ['id'];
                     }
                 ];
             };
         };
         Views: {
-            [_ in never]: never
+            [_ in never]: never;
         };
         Functions: {
             install_available_extensions_and_test: {
@@ -136,90 +162,96 @@ export interface Database {
             };
         };
         Enums: {
-            [_ in never]: never
+            [_ in never]: never;
         };
         CompositeTypes: {
-            [_ in never]: never
+            [_ in never]: never;
         };
     };
 }
 
 export type Tables<
     PublicTableNameOrOptions extends
-    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
-    | { schema: keyof Database; },
-    TableName extends PublicTableNameOrOptions extends { schema: keyof Database; }
-    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never
-> = PublicTableNameOrOptions extends { schema: keyof Database; }
-    ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-            Row: infer R;
-        }
-    ? R
-    : never
-    : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
-        Database["public"]["Views"])
-    ? (Database["public"]["Tables"] &
-        Database["public"]["Views"])[PublicTableNameOrOptions] extends {
-            Row: infer R;
-        }
-    ? R
-    : never
+        | keyof (Database['public']['Tables'] & Database['public']['Views'])
+        | { schema: keyof Database },
+    TableName extends PublicTableNameOrOptions extends {
+        schema: keyof Database;
+    }
+        ? keyof (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+              Database[PublicTableNameOrOptions['schema']]['Views'])
+        : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+    ? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+          Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
+          Row: infer R;
+      }
+        ? R
+        : never
+    : PublicTableNameOrOptions extends keyof (Database['public']['Tables'] &
+          Database['public']['Views'])
+    ? (Database['public']['Tables'] &
+          Database['public']['Views'])[PublicTableNameOrOptions] extends {
+          Row: infer R;
+      }
+        ? R
+        : never
     : never;
 
 export type TablesInsert<
     PublicTableNameOrOptions extends
-    | keyof Database["public"]["Tables"]
-    | { schema: keyof Database; },
-    TableName extends PublicTableNameOrOptions extends { schema: keyof Database; }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
-> = PublicTableNameOrOptions extends { schema: keyof Database; }
-    ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-        Insert: infer I;
+        | keyof Database['public']['Tables']
+        | { schema: keyof Database },
+    TableName extends PublicTableNameOrOptions extends {
+        schema: keyof Database;
     }
-    ? I
-    : never
-    : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-    ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I;
-    }
-    ? I
-    : never
+        ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+        : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+    ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+          Insert: infer I;
+      }
+        ? I
+        : never
+    : PublicTableNameOrOptions extends keyof Database['public']['Tables']
+    ? Database['public']['Tables'][PublicTableNameOrOptions] extends {
+          Insert: infer I;
+      }
+        ? I
+        : never
     : never;
 
 export type TablesUpdate<
     PublicTableNameOrOptions extends
-    | keyof Database["public"]["Tables"]
-    | { schema: keyof Database; },
-    TableName extends PublicTableNameOrOptions extends { schema: keyof Database; }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
-> = PublicTableNameOrOptions extends { schema: keyof Database; }
-    ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-        Update: infer U;
+        | keyof Database['public']['Tables']
+        | { schema: keyof Database },
+    TableName extends PublicTableNameOrOptions extends {
+        schema: keyof Database;
     }
-    ? U
-    : never
-    : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-    ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U;
-    }
-    ? U
-    : never
+        ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+        : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+    ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+          Update: infer U;
+      }
+        ? U
+        : never
+    : PublicTableNameOrOptions extends keyof Database['public']['Tables']
+    ? Database['public']['Tables'][PublicTableNameOrOptions] extends {
+          Update: infer U;
+      }
+        ? U
+        : never
     : never;
 
 export type Enums<
     PublicEnumNameOrOptions extends
-    | keyof Database["public"]["Enums"]
-    | { schema: keyof Database; },
-    EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database; }
-    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never
-> = PublicEnumNameOrOptions extends { schema: keyof Database; }
-    ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-    : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
-    ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+        | keyof Database['public']['Enums']
+        | { schema: keyof Database },
+    EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+        ? keyof Database[PublicEnumNameOrOptions['schema']]['Enums']
+        : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
+    : PublicEnumNameOrOptions extends keyof Database['public']['Enums']
+    ? Database['public']['Enums'][PublicEnumNameOrOptions]
     : never;
