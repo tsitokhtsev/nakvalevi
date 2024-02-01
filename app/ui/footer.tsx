b61706a8a1e45b98e72568511f82980c7bc51fac
Grigoli Tsitokhtsevi <tsitokhtsev2002@gmail.com>
2024-02-01 18:10:22 +0400

    NAK-50 Add conteiner to header and footer

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer() {
     return (
-        <footer className="mx-auto p-4 max-w-screen-xl">
-            ნაკვალევი {new Date().getFullYear()}
+        <footer className="w-full bg-white">
+            <div className="container flex items-center justify-center py-4">
+                <span className="text-green-darker">
+                    ნაკვალევი {new Date().getFullYear()}
+                </span>
+            </div>
         </footer>
     );
 }
--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import Link from 'next/link';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const links = [
     { name: 'მთავარი', href: '/' },
@@ -11,24 +11,30 @@ const links = [
 
 export default function Header() {
     return (
-        <header className="flex justify-between items-center mx-auto p-4 max-w-screen-xl">
-            <Link href="/">
-                <Image
-                    src="/images/logo.svg"
-                    alt="ნაკვალევის ლოგო"
-                    width={48}
-                    height={48}
-                />
-            </Link>
-            <nav className="flex gap-6">
-                {links.map((link) => {
-                    return (
-                        <Link key={link.name} href={link.href}>
-                            {link.name.toUpperCase()}
-                        </Link>
-                    );
-                })}
-            </nav>
+        <header className="fixed top-0 w-full bg-white">
+            <div className="container flex items-center justify-between py-4">
+                <Link href="/">
+                    <Image
+                        src="/images/logo.svg"
+                        alt="ნაკვალევის ლოგო"
+                        width={48}
+                        height={48}
+                    />
+                </Link>
+                <nav className="flex gap-8">
+                    {links.map((link) => {
+                        return (
+                            <Link
+                                key={link.name}
+                                href={link.href}
+                                className="text-green-darker"
+                            >
+                                {link.name.toUpperCase()}
+                            </Link>
+                        );
+                    })}
+                </nav>
+            </div>
         </header>
     );
 }
