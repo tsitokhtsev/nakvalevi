c6ba993b4f4bbd65711b949869f783f990610764
Grigoli Tsitokhtsevi <tsitokhtsev2002@gmail.com>
2024-02-08 17:10:18 +0400

    NAK-55 Implement Writings Page

--- app/writings/page.tsx
+++ app/writings/page.tsx
@@ -1,5 +1,32 @@
+import { createSupabaseServerClient } from '@/app/lib/supabase';
 import Heading from '@/app/ui/heading';
+import WritingCard from '@/app/ui/writing-card';
 
-export default function Page() {
-    return <Heading text="ნაწარმოებები" />;
+export default async function Page() {
+    const supabase = createSupabaseServerClient();
+    const { data: writings } = await supabase
+        .from('writings')
+        .select('*, author:authors(*), genres(*)')
+        .order('id');
+    const { data: author } = await supabase
+        .from('authors')
+        .select('*')
+        .order('id');
+
+    return (
+        <div className="container">
+            <Heading text="ნაწარმოებები" classes="mb-4" />
+            {writings ? (
+                <div className="flex flex-col gap-8">
+                    {writings.map((writing) => {
+                        return (
+                            <WritingCard key={writing.id} writing={writing} />
+                        );
+                    })}
+                </div>
+            ) : (
+                <p className="text-black">არცერთი ნაწარმოები არ მოიძებნა...</p>
+            )}
+        </div>
+    );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: 'ahvmvkeajbesfkbymnhn.supabase.co',
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
