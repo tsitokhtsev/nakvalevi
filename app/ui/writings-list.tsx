3875d7a0edb63c9850050ea7b524fd2eb2b9abaa
Grigoli Tsitokhtsevi <tsitokhtsev2002@gmail.com>
2024-02-15 14:33:44 +0400

    NAK-53 Implement search using filtering

--- app/ui/writings-list.tsx
+++ app/ui/writings-list.tsx
@@ -1,16 +1,36 @@
 'use client';
 
+import { useState } from 'react';
+
 import { Writing } from '@/app/lib/definitions';
-import Search from '@/app/ui/search';
 import WritingCard from '@/app/ui/writing-card';
 
 export default function WritingsList({ writings }: { writings: Writing[] }) {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const filteredWritings = writings.filter((writing) => {
+        return (
+            writing.title.includes(searchTerm) ||
+            writing?.author?.name.includes(searchTerm)
+        );
+    });
+
     return (
         <div>
-            {/* <Search /> */}
+            <div>
+                <label htmlFor="search" className="sr-only">
+                    ძიება
+                </label>
+                <input
+                    className="mb-8 w-full rounded-3xl border-2 border-brown px-8 py-4"
+                    placeholder="დაიწყე ძიება..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
             <div className="flex flex-col gap-8">
-                {writings.length ? (
-                    writings.map((writing) => (
+                {filteredWritings.length ? (
+                    filteredWritings.map((writing) => (
                         <WritingCard key={writing.id} writing={writing} />
                     ))
                 ) : (
