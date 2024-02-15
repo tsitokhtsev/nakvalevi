23ee6316bc427d2ecedb6fafa88a7e1b44cbe4b3
Grigoli Tsitokhtsevi <tsitokhtsev2002@gmail.com>
2024-02-15 20:43:20 +0400

    NAK-56 Add main info

--- app/writings/[id]/page.tsx
+++ app/writings/[id]/page.tsx
@@ -1,5 +1,8 @@
 import { fetchWritingById, fetchWritings } from '@/app/lib/data';
 import Heading from '@/app/ui/heading';
+import WritingAuthor from '@/app/ui/writing-author';
+import WritingPills from '@/app/ui/writing-pills';
+import Year from '@/app/ui/year';
 
 export async function generateStaticParams() {
     const writings = await fetchWritings();
@@ -10,11 +13,16 @@ export async function generateStaticParams() {
 
 export default async function Page({ params }: { params: { id: string } }) {
     const writing = await fetchWritingById(params.id);
-    const { title } = writing;
+    const { title, author, year, period, genres } = writing;
 
     return (
         <div className="container">
             <Heading text={title} />
+            <div className="mt-8 flex flex-col gap-4 rounded-2xl bg-brown-lighter p-8 md:flex-row md:items-center md:gap-8 md:rounded-3xl">
+                <WritingAuthor author={author!} />
+                <Year year={year} />
+                <WritingPills period={period} genres={genres} />
+            </div>
         </div>
     );
 }
